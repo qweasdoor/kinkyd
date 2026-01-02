6b0a8e9889a0abe00fc45cd1a9f28d175e2021d0
qweasdoor <qweasdoor@gmail.com>
2026-01-02 10:27:39 +0700

    Update config.js

--- capcut-kink/config/config.js
+++ capcut-kink/config/config.js
@@ -27,10 +27,12 @@ export const CONFIG = {
       SIGNUP_BUTTON: '.lv_sign_in_panel_wide-sign-in-button', // Tombol daftar/lanjut setelah password
 
       // Birthday UI
-    BIRTHDAY_INPUT: 'input.gate_birthday-picker-input', 
-    BIRTHDAY_MONTH_SELECTOR: 'div[role="combobox"]:nth-of-type(1)', 
-    BIRTHDAY_DAY_SELECTOR: 'div[role="combobox"]:nth-of-type(2)',
+    BIRTHDAY_INPUT: 'input.gate_birthday-picker-input',
+    BIRTHDAY_MONTH_SELECTOR: '.gate_birthday-picker-selector:nth-of-type(1)',
+    BIRTHDAY_DAY_SELECTOR: '.gate_birthday-picker-selector:nth-of-type(2)',
     BIRTHDAY_NEXT_BUTTON: '.lv_sign_in_panel_wide-primary-button',
+    OTP_INPUT: 'input.lv-input',
+    DROPDOWN_ITEMS: '.lv-select-popup li'
     },
   },
 
@@ -83,6 +85,7 @@ export const CONFIG = {
     DEFAULT_PASSWORD: 'keep123',
   },
 };
+
 
 
 
